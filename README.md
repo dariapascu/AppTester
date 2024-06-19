b1330a71fc7a43a673669ca551b7641bb4738e95
dariapascu <datia.pascu@mta.ro>
2024-06-19 12:49:11 +0300

    commit2

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # AppTester
 Script pentru testarea unei aplicații(timp de execuție, input-output, apeluri de sistem(strace), apeluri de biblioteca(ltrace). Modularizat.
 
-# 18.06.2023
+# 18.06.2024
 -am ales sa testez aplicatia GIMP (un editor foto) deoarece are multe functionalitati a caror timpi de executie, apeluri de sistem si de biblioteci pot varia in functie de modificarile facute
 -am instalat aplicatia si m-am documentat despre cum poate fi actionata si din linie de comanda
 -am ales o poza careia sa ii aplic un filtru alb-negru si am masurat timpul de executie al acestei actiuni
+
+# 19.06.2024
+-am organizat codul astfel incat sa fie mai usor de inteles si de utilizat
+-am realizat scrierea in fisiere de log a apelurilor de sistem si biblioteci pe care le-am obtinut la aplicarea filtrului black and white
+-am folosit ImageMagick pentru a testa si in cadrul scriptului daca imaginea rezultata la output este in alb si negru, pentru a nu fi nevoie sa se acceseze imaginea pentru verificarea rfezultatului
--- script.sh
+++ script.sh
@@ -2,45 +2,84 @@
 
 if ! command -v gimp &> /dev/null
 then
-    echo "GIMP nu este instalat. Instalează-l înainte de a rula acest script."
+    echo "GIMP nu este instalat."
     exit 1
 fi
-
 
 if ! command -v strace &> /dev/null
 then
-    echo "strace nu este instalat. Instalează-l înainte de a rula acest script."
+    echo "strace nu este instalat."
     exit 1
 fi
-
 
 if ! command -v ltrace &> /dev/null
 then
-    echo "ltrace nu este instalat. Instalează-l înainte de a rula acest script."
+    echo "ltrace nu este instalat."
     exit 1
 fi
 
-input_file="mojito.png"
-output_file="output_image.png"
+bwfilter() {
+    local input_file="$1"
+    local output_file="$2"
 
-if [ ! -f "$input_file" ]; then
-    echo "Fișierul de intrare $input_file nu există."
-    exit 1
-fi
+    start_time=$(date +%s.%N)
 
-start_time=$(date +%s.%N)
+    # aplicarea unui filtru alb-negru
+    gimp_batch_commands="(let* (
+      (image (car (gimp-file-load RUN-NONINTERACTIVE \"$PWD/$input_file\" \"$PWD/$input_file\")))
+      (drawable (car (gimp-image-get-active-layer image))))
+     (gimp-drawable-desaturate drawable 0)
+     (gimp-file-save RUN-NONINTERACTIVE image drawable \"$PWD/$output_file\" \"$PWD/$output_file\")
+     (gimp-quit 0))"
 
-# aplicarea unui filtru alb-negru
-gimp_batch_commands="(let* (
-  (image (car (gimp-file-load RUN-NONINTERACTIVE \"$PWD/$input_file\" \"$PWD/$input_file\")))
-  (drawable (car (gimp-image-get-active-layer image))))
- (gimp-drawable-desaturate drawable 0)
- (gimp-file-save RUN-NONINTERACTIVE image drawable \"$PWD/$output_file\" \"$PWD/$output_file\")
- (gimp-quit 0))"
+    gimp -i -b "$gimp_batch_commands" &> /dev/null
 
-gimp -i -b "$gimp_batch_commands"
+    end_time=$(date +%s.%N)
+    execution_time=$(echo "$end_time - $start_time" | bc)
+    echo "Timpul total de executie: $execution_time secunde"
 
-end_time=$(date +%s.%N)
-execution_time=$(echo "$end_time - $start_time" | bc)
-echo "Timpul total de execuție: $execution_time secunde"
+    identify_output=$(identify -verbose "$output_file")
+
+    if echo "$identify_output" | grep -i "Type: Grayscale" &> /dev/null
+    then
+        echo "Imaginea este alb-negru."
+    else
+        echo "Imaginea nu este alb-negru."
+    fi
+
+    echo "Se scriu apelurile de sistem in strace.log..."
+    strace -o stracebw.log -e trace=file,read,write gimp -i -b "$gimp_batch_commands" &> /dev/null
+
+    echo "Se scriu apelurile de biblioteci in ltrace.log..."
+    ltrace -o ltracebw.log gimp -i -b "$gimp_batch_commands" &> /dev/null
+
+    echo "Operatie finalizata!"
+}
+
+PS3="Alegeti dintre optiunile de mai jos:"
+select ITEM in "Aplicare filtru black and white" "Exit"
+do
+	case $REPLY in
+		1) echo "Introduceti numele fisierului pe care doriti sa il modificati: "
+            read input_file
+            if [ ! -f "$input_file" ]
+            then
+                echo "Fisierul de intrare $input_file nu exista."
+                exit 1
+            fi
+
+            file_type=$(file -b --mime-type "$input_file" | egrep -i "image")
+            if [[ ! -n "$file_type" ]]
+            then
+                echo "Fisierul furnizat nu e o imagine"
+                exit 1
+            fi
+
+            output_file="output_bw.png"
+            bwfilter "$input_file" "$output_file"
+		;;
+		2) exit 0 ;;
+		*) echo "Optiune inexistenta"
+	esac
+done
 
